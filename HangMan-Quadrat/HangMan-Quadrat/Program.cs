8250322ddff213179b15d9443cbf148173d3501d
Evermann <Evermann-P@schueler.ers-hm.de>
2018-02-07 09:48:56 +0100

    input possible

--- HangMan-Quadrat/HangMan-Quadrat/Program.cs
+++ HangMan-Quadrat/HangMan-Quadrat/Program.cs
@@ -8,8 +8,56 @@ namespace HangMan_Quadrat
 {
     class Program
     {
+        static string wordToGuess = "";
+        static int errorCount = 0;
+
+        static bool isRunning = true;
+
+        static char[] alphabet;
+        static char[] errorAlphabet;
+
         static void Main(string[] args)
         {
+            alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+            Console.WriteLine("Grüß dich.\nBitte gebe dein zu erratenes Wort ein.");
+            wordToGuess = Console.ReadLine().ToUpper();
+            Console.Clear();
+            DrawUnderline();
+
+            while (isRunning)
+            {
+                RunGame();
+            }
+            
+
+
+
+            // last line in code...
+            Console.ReadLine();
+        }
+        static void DrawUnderline()
+        {
+            foreach (char letter in wordToGuess)
+            {
+                Console.Write("_ ");
+            }
+            Console.Write("\n");
+            
+        }
+        
+        static bool RunGame()
+        {
+            //ask for Letter
+
+            //check if letter was guessed before
+
+            //check if letter is in wordToGuess
+
+            //check if game has ended -> vicoty / lose
+
+            if (victory || lose) return false;
+            return true;
         }
     }
 }
