a3fe1ba360c3d35457411e0d84b5ce9a2b12594c
Gramini <Evermann-P@schueler.ers-hm.de>
2018-02-09 12:52:17 +0100

    spiel kann gewonnen werden

--- HangMan-Quadrat/Program.cs
+++ HangMan-Quadrat/Program.cs
@@ -17,6 +17,11 @@ namespace HangMan_Quadrat
 
         static char[] alphabet;
         static string triedLetters = "";
+        static bool[] progress;
+        private static bool victory = false;
+
+        static ConsoleColor endColor = ConsoleColor.Red;
+        static string endMessage = "Noob!";
 
         static void Main(string[] args)
         {
@@ -27,28 +32,47 @@ namespace HangMan_Quadrat
             Console.WriteLine("Grüß dich.\n");
             wordToGuess = Input.GetInput(@"^[a-zA-Z-]{1,}$", "Bitte gebe dein zu erratenes Wort ein: ", "Eingabe fehlerhaft").ToUpper();
 
+            progress = new bool[wordToGuess.Length];
+
             Console.Clear();
-            DrawUnderline();
+            DrawWord();
 
             while (isRunning)
             {
                 RunGame();
             }
-            
+
+            #region draw end message
+            Console.ForegroundColor = endColor;
+            Console.WriteLine(endMessage);
+            #endregion
 
 
 
             // last line in code...
             Console.ReadLine();
         }
-        static void DrawUnderline()
+
+        static void DrawWord()
         {
-            foreach (char letter in wordToGuess)
+            for (int i = 0; i < wordToGuess.Length; i++)
             {
+                if (progress[i])
+                {
+                    Console.Write(wordToGuess[i] + " ");
+                    continue;
+                }
                 Console.Write("_ ");
             }
+
             Console.Write("\n");
-            
+
+            if (!progress.Contains(false))
+            {
+                victory = true;
+                endColor = ConsoleColor.Green;
+                endMessage = "Gewonnen!";
+            }
         }
         
         static void RunGame()
@@ -67,12 +91,25 @@ namespace HangMan_Quadrat
             triedLetters += inputLetter;
 
             //check if letter is in wordToGuess
+            errorCount++;
+            if (wordToGuess.IndexOf(inputLetter) != -1)
+            {
+                for (int i = 0; i < wordToGuess.Length; i++)
+                {
+                    if (wordToGuess[i].ToString() == inputLetter)
+                    {
+                        progress[i] = true;
+                        errorCount--;
+                    }
+                }
+            }
 
+            Console.Clear();
+
+            DrawWord();
 
             //check if game has ended -> vicoty / lose
-
-            //if (victory || lose) isRunning = false;;
-
+            if (victory || errorCount >= 8) isRunning = false;
         }
 
         #region Input
