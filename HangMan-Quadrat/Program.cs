e5ed2128971c2a617a19c8dd92c495cbd13c3ff6
Gramini <Evermann-P@schueler.ers-hm.de>
2018-02-12 12:25:48 +0100

    wort erraten + ausgabe fehlercounter

--- HangMan-Quadrat/Program.cs
+++ HangMan-Quadrat/Program.cs
@@ -55,6 +55,8 @@ namespace HangMan_Quadrat
 
         static void DrawWord()
         {
+            Console.ForegroundColor = ConsoleColor.Gray;
+
             for (int i = 0; i < wordToGuess.Length; i++)
             {
                 if (progress[i])
@@ -65,7 +67,18 @@ namespace HangMan_Quadrat
                 Console.Write("_ ");
             }
 
-            Console.Write("\n");
+            #region Draw Error Count
+            string errorText = errorCount.ToString() + " Fehler!";
+            int cursorOffset = Console.BufferWidth - Math.Max(errorText.ToString().Length, 1);
+            Console.CursorLeft = cursorOffset;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write(errorText);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.BackgroundColor = ConsoleColor.Black;
+            #endregion
+
+            Console.WriteLine("\n");
 
             if (!progress.Contains(false))
             {
@@ -77,22 +90,49 @@ namespace HangMan_Quadrat
         
         static void RunGame()
         {
+            //check if game has ended -> vicoty / lose
+            if (victory || errorCount >= 8)
+            {
+                isRunning = false;
+                Console.WriteLine("Spiel vorbei!");
+                return;
+            }
+
             //ask for Letter
-            string inputLetter = Input.GetInput(@"^[a-zA-Z]{1}$", "Eingabe eines neuen Buchstabens: ", "Die Eingabe darf nur ein Buchstabe sein!");
+            string inputLetter = Input.GetInput(@"^[a-zA-Z]{1,}$", "Eingabe eines neuen Buchstabens: ", "Die Eingabe darf nur ein Buchstabe sein!");
+
+            if (inputLetter.Length > 1)
+            {
+                if (!HandleWord(inputLetter))
+                {
+                    errorCount += 2;
+
+                    return;
+                }
+
+                victory = true;
+                Console.WriteLine("Das Wort '" + wordToGuess + "' wurde erraten! Glückwunsch");
+                return;
+            }
 
             //check if letter was guessed before
             if (triedLetters.Contains(inputLetter))
             {
-                Console.WriteLine("'" + inputLetter + "'" + " wurde bereits geraten");
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("'" + inputLetter + "' wurde bereits geraten");
                 errorCount++;
+                DrawWord();
                 return;
             }
 
             triedLetters += inputLetter;
 
+            Console.Clear();
+
             //check if letter is in wordToGuess
             errorCount++;
-            if (wordToGuess.IndexOf(inputLetter) != -1)
+            if (wordToGuess.Contains(inputLetter))
             {
                 for (int i = 0; i < wordToGuess.Length; i++)
                 {
@@ -103,13 +143,24 @@ namespace HangMan_Quadrat
                     }
                 }
             }
-
-            Console.Clear();
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(inputLetter + " ist nicht enthalten!");
+            }
 
             DrawWord();
 
-            //check if game has ended -> vicoty / lose
-            if (victory || errorCount >= 8) isRunning = false;
+        }
+
+        private static bool HandleWord(string input)
+        {
+            if (input == wordToGuess)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         #region Input
